d841c2d3ef793f8023f702f061a853c8e45f6654
30Elena <lenayrakova@gmail.com>
2025-01-26 20:03:38 +0400

    Add files via upload

--- mail.py
+++ mail.py
@@ -4,13 +4,13 @@ import os
 
 load_dotenv()
 
-letter = ("""\
+letter = """\
 From: {sender}
 To: {recipient}
 Subject: {title}
 Content-Type: {content_type}
 
-{message}""")
+{message}"""
 
 sender = 'iotasamurai@ya.ru'
 recipient = 'iotasamurai@ya.ru'
@@ -18,7 +18,20 @@ title = 'Приглашение!'
 content_type = 'text/plain; charset="UTF-8";'
 message = """Привет, %friend_name%! %my_name% приглашает тебя на сайт %website%!
 
-dvmn.org — это новая версия онлайн-курса..."""
+%website% — это новая версия онлайн-курса по программированию. 
+Изучаем Python и не только. Решаем задачи. Получаем ревью от преподавателя. 
+
+Как будет проходить ваше обучение на %website%? 
+
+→ Попрактикуешься на реальных кейсах. 
+Задачи от тимлидов со стажем от 10 лет в программировании.
+→ Будешь учиться без стресса и бессонных ночей. 
+Задачи не «сгорят» и не уйдут к другому. Занимайся в удобное время и ровно столько, сколько можешь.
+→ Подготовишь крепкое резюме.
+Все проекты — они же решение наших задачек — можно разместить на твоём GitHub. Работодатели такое оценят. 
+
+Регистрируйся → %website%  
+На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл."""
 
 letter = letter.format(sender=sender, 
 	recipient=recipient, 
@@ -29,10 +42,8 @@ letter = letter.format(sender=sender,
 letter = letter.replace('%website%', 'https://dvmn.org/profession-ref-program/lenayrakova/l7XDv/')
 letter = letter.replace('%friend_name%', 'Александр')
 letter = letter.replace('%my_name%', 'Елена')
-print(letter)
 
 letter = letter.encode("UTF-8")
-print(letter)
 
 server = smtplib.SMTP_SSL('smtp.yandex.ru', 465)
 login = os.getenv('login')
